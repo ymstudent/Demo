ac3c26d17c80772f0cdb1b1fc9f6ef92a7830db0
ymstudent <m13213127921@163.com>
2018-01-06 15:45:46 +0800

    单例模式

--- pattern/Core/DataBase.php
+++ pattern/Core/DataBase.php
@@ -4,7 +4,7 @@ namespace Ym\Demo\Pattern;
 /**
  * 利用单例模式实现一个数据库连接
  * 单例模式的要点：1、一个保存类实例的私有、静态成员变量；2、一个私有的构造方法；3、用来实例化本身的公有接口
- * 单例模式的好处：1、避免大量new操作消耗资源；2、需要更改设置时，只需要在一处更改即可
+ * 单例模式的好处：1、避免大量new操作消耗资源；2、需要更改设置时，只需要在一处修改，处处生效
  * Class DataBase
  * @package Ym\Demo\pattern
  */
@@ -19,7 +19,7 @@ class DataBase
         self::$db = mysqli_connect('127.0.0.1','root', '', '');
     }
 
-    //实例化类的方法
+    //实例化类的公共接口
     public static function getInstance()
     {
         if (empty(self::$db)) {
